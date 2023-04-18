9c04f54d37af6a72edd549b3a7e63381b24c256d
Dodi Yulian <cdoditri@gmail.com>
2023-04-18 09:54:25 +0700

    add livewire

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
   <link rel="preconnect" href="https://fonts.bunny.net">
   <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+  @livewireStyles
   <!-- Scripts -->
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -197,12 +198,8 @@
         {{ $slot }}
       </div>
     </div>
-
-    <!-- Page Content -->
-    <main>
-
-    </main>
   </div>
+  @livewireScripts
 </body>
 
 </html>
